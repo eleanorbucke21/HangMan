52bd1c28a990bab43659dedc7300aa4872e45490
Eleanor Bucke <rubybucke@live.com>
2022-10-10 07:54:30 +0000

    Add visual hangman

--- run.py
+++ run.py
@@ -5,8 +5,9 @@
 import random
 from words import words
 import string
+from visual import hangman_visual
 
-name = input('Player name?')
+name = input('Player name?').upper()
 print('Hi', name, 'Lets play Hangman!')
 
 
@@ -31,13 +32,15 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
+    score = 0
 
     lives = 6
 
     while len(word_letters) > 0 and lives > 0:
-        print('You have', lives, 'lives left. Letters already used: ', ' '.join(used_letters))
+        print(name,',you have', lives, 'lives left. Letters already used: ', ' '.join(used_letters))
 
         word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(hangman_visual[lives])
         print('Current word:', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
@@ -48,7 +51,7 @@ def hangman():
 
             else:
                 lives = lives - 1
-                print('It is not there, try again')
+                print('It is not there,',name,'try again')
 
         elif user_letter in used_letters:
             print('You already used. Try again')
@@ -58,8 +61,11 @@ def hangman():
 
     if lives == 0:
         print('No more lives. The word was', word, '!!!')
+        print('Your score is',score)
     else:
+        score = + 1
         print('You got it!')
+        print('Your score is',score)
 
 
 hangman()
